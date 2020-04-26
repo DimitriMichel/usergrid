b1673f28fcd8013f511a718023c203a50609f02a
Dimitri PL <dimitrimachines@gmail.com>
2020-04-26 04:28:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
+import "./App.css";
 
-function App() {
+const UserGrid = ({ users, setUserIndex }) => {
+  const userList = users.map((user, index) => {
+    return (
+      <motion.li
+        className="user-item"
+        key={index}
+        layoutId={index}
+        onClick={() => setUserIndex(index)}
+      >
+        <img
+          className="user-images"
+          src={user.picture.large}
+          alt={`${user.name.first}`}
+        />
+        {user.name.first + " " + user.name.last}
+      </motion.li>
+    );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul className="user">{userList}</ul>
     </div>
   );
-}
+};
+
+const UserTile = ({ users, onClick }) => {
+  return (
+    <div className="single-user-container" onClick={onClick}>
+      <motion.div></motion.div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const [index, setUserIndex] = useState(false);
+
+  useEffect(() => {
+    fetch(`https://randomuser.me/api/?results=16`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setUsers(data.results);
+      });
+  }, []);
+  console.log(users);
+  console.log(users);
+
+  return (
+    <div>
+      <UserGrid users={users} setUserIndex={setUserIndex} />
+      <UserTile
+        users={users}
+        index={index}
+        user={users[index]}
+        setIndex={setUserIndex}
+        onClick={() => setUserIndex(false)}
+      />
+    </div>
+  );
+};
 
 export default App;
